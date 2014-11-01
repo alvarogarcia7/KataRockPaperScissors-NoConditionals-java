5061869b1106f5981aecd662aa8c6f1b23a23a3c
Alvaro Garcia <derecerca@gmail.com>
2014-11-01 19:14:18 +0100

    green

--- src/RockPaperScissorsTest.java
+++ src/RockPaperScissorsTest.java
@@ -1,9 +1,25 @@
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 
 public class RockPaperScissorsTest {
 	
+	public enum Gesture {
+		Rock, Paper;
+
+		public Object against(Gesture paper2) {
+			return Result.LOSE;
+		}
+
+	}
+
+	public enum Result {
+		LOSE
+
+	}
+
 	@Test
 	public void rockAgainstPaper_should_lose(){
 		assertEquals(Result.LOSE, Gesture.Rock.against(Gesture.Paper));
